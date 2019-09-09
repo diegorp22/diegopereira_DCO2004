4762d4f8a4d06685430b23e061b1337a7b718995
diegorp22 <diegorp22@gmail.com>
2019-09-09 01:11:47 -0300

    att

---
+++ Hands-on 00/HD00-Pratica01.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# # Exercício da prática 1  
+
+# **O Processo de modulação explora os três parâmetros da senoide portadora:** 
+# _Amplitude, Frequência e Fase_
+# [aqui](https://pt.wikipedia.org/wiki/Modula%C3%A7%C3%A3o_em_amplitude)
+# ![fig_figura](https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Amplitude-modulation.png/250px-Amplitude-modulation.png)
+
+# In[1]:
+
+a = int(input("Escolha a primeira variável"))
+b = int(input("Escolha a segunda variável"))
+print("A soma das variáveis é:", a+b)
+
+"Prática realizada."
