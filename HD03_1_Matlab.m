d075eb18856afafb1f11678584207b0e102d2e9f
diegorp22 <diegorp22@gmail.com>
2019-09-11 16:26:14 -0300

    att

---
+++ HD03_1_Matlab.m
@@ -0,0 +1,44 @@
+close all;clc;clear all;                     % Limpa variáveis e fecha todos os gráficos
+FlautaFile = ['Flauta.wav'];                 % Especifica do local e nome do arquivo de áudio
+ViolinoFile = ['Violino.wav'];               % Especifica do local e nome do arquivo de áudio
+
+[vtFlauta,  fs1] = audioread(FlautaFile);    % Abre arquivo de áudio de um arquivo
+[vtViolino, fs2] = audioread(ViolinoFile);   % Abre arquivo de áudio de um arquivo
+
+%% FFT
+
+T = 1/fs1;             % Sampling period       
+L1 = length(vtFlauta);  % Length of signal
+t1 = (0:L1-1)*T;         % Time vector
+L2 = length(vtViolino);  % Length of signal
+t2 = (0:L2-1)*T;         % Time vector
+
+Y1 = fft(vtFlauta);
+Y2 = fft(vtViolino);
+
+%Flauta
+Pa1 = abs(Y1/L1);
+Pb1 = Pa1(1:L1/2+1);
+Pb1(2:end-1) = 2*Pb1(2:end-1);
+
+f1 = fs1*(0:(L1/2))/L1;
+
+%Violino
+Pa2 = abs(Y2/L2);
+Pb2 = Pa2(1:L2/2+1);
+Pb2(2:end-1) = 2*Pb2(2:end-1);
+
+f2 = fs2*(0:(L2/2))/L2;
+
+%Plot
+subplot(2,1,1);
+plot(f1,Pb1)
+title('Espectro da Flauta')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+subplot(2,1,2);
+plot(f2,Pb2)
+title('Espectro do Violino')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
---
+++ HD03_Matlab.m
@@ -0,0 +1,39 @@
+close all;clc;clear all;                     % Limpa variáveis e fecha todos os gráficos
+FlautaFile = ['Flauta.wav'];                 % Especifica do local e nome do arquivo de áudio
+ViolinoFile = ['Violino.wav'];               % Especifica do local e nome do arquivo de áudio
+
+[vtFlauta,  fs1] = audioread(FlautaFile);    % Abre arquivo de áudio de um arquivo
+[vtViolino, fs2] = audioread(ViolinoFile);   % Abre arquivo de áudio de um arquivo
+
+%% FFT
+fs=fs1;                      % Frequência de amostragem em kHz
+
+N1=length(vtFlauta);         % Número de amostras
+N1=4*N1;                     % Número de pontos da fft
+
+N2=length(vtViolino);        % Número de amostras
+N2=4*N2;                     % Número de pontos da fft
+
+f1=(0:N1-1)*fs/N1;           % Definição do eixo da frequência [axis]
+f2=(0:N2-1)*fs/N2;           % Definição do eixo da frequência [axis]
+
+Xf=abs(fft(vtFlauta,N1));    % Encontra a magnitude da FFT usando janela retangular (sem janela)
+Xv=abs(fft(vtViolino,N2));   % Encontra a magnitude da FFT usando janela retangular (sem janela)
+%% Plot
+% Flauta
+subplot(2,1,1);
+plot(f1(1:N1/2),20*log10(Xf(1:N1/2)/max(Xf)));
+title('Espectro da Flauta');
+grid;
+xlabel('Frequência, kHz');
+ylabel('Magnitude, [dB]');
+axis([f1(1) f1(N1/2) -150 0]);
+
+% Violino
+subplot(2,1,2);
+plot(f2(1:N2/2),20*log10(Xv(1:N2/2)/max(Xv)));
+title('Espectro do Violino');
+grid;
+xlabel('Frequência, kHz');
+ylabel('Magnitude, [dB]');
+axis([f2(1) f2(N2/2) -150 0]);
